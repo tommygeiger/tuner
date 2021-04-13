18524d111ae312d90f17d599c9f39c1fec2d454f
Thomas Geiger <tommyhawk21@gmail.com>
2021-04-13 04:50:22 -0400

    Remove callback uri for gh-pages

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "3cf6931c4f974651b57c25157fe28427";
-export const redirectUri = "https://tommygeiger.com/spotify-react-app/callback";
+export const redirectUri = "https://tommygeiger.com/spotify-react-app/";
 export const scopes = [
     "user-read-private",
     "user-read-email"
